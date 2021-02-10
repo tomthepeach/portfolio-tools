586ae6e7fdcab8b179066d5d956ccc4b989176b2
tomthepeach <77214385+tomthepeach@users.noreply.github.com>
2021-02-10 15:33:27 +0000

    Add files via upload

--- Rebalance.py
+++ Rebalance.py
@@ -1,106 +1,109 @@
 import pandas as pd
 from pandas_datareader import data as web
 import numpy as np
-import matplotlib.pyplot as plt
 import datetime
-from IPython.display import display, HTML
-import re
 from forex_python.converter import CurrencyRates
-import matplotlib.ticker as ticker
 
 
+# Date-time considerations
 today = datetime.datetime.today()
 yesterday = today - datetime.timedelta(days=1)
 
+# List of average annual inflation values (defunct)
+# RPI = [item for sublist in pd.read_csv('C:/Users/Tom/Desktop/ukrpi.csv').values for item in sublist]
 
-RPI = [item for sublist in pd.read_csv('C:/Users/Tom/Desktop/ukrpi.csv').values for item in sublist]
+# Reading the da
+myassets = pd.read_csv(r'C:\Users\Tom\Desktop\Python projects\Portfolio management tool\assets.csv',
+                       index_col=False,
+                       dtype=str)
 
 
-gold = ['SGLN.L#30/p']
-emerg = ['HMCA.L#30/£', 'HMCH.L#60/p', 'HTWN.L#5/p']
-bond = ['VGOV.L#30/£']
-euvalue = ['ZPRX.DE#30/e']
+# Function to add an asset into the asset table requiring the ticker, number of shares and currency
+def add_asset(ticker, shares, currency):
 
-myassets = {'gold': ['SGLN.L#30/p'], 'emerg': ['HMCA.L#30/£', 'HMCH.L#60/p', 'HTWN.L#5/p'], 'bond': ['VGOV.L#30/£'], 'euvalue': ['ZPRX.DE#30/e'], 'volatlitiy': ['^VIX#1/£']}
+    print("What asset class is this? We currently have:" + str([key for key, value in myassets.items()]))
+    asset_class = input()
+    myassets[asset_class].append(f'{ticker}#{shares}/{currency}')
+    df = pd.concat([pd.Series(y,name=x) for x,y in myassets.items()],axis =1)
+    df.to_csv(r'C:\Users\Tom\Desktop\Python projects\Portfolio management tool\assets.csv',
+              index=False)
 
-# Managing exchange rates
 
-symbtotick = {'£': 'GBP', 'e': 'EUR', '$': 'USD'}  # A dictionary to hold currency tickers
-
-def exch(cur1, cur2):  # Function to fetch the exchange rate for a currency pair
+# Function to fetch the exchange rate for a currency pair
+def exch(cur1, cur2):
 
     c = CurrencyRates()
     return c.get_rate(cur1, cur2)
 
+
+# Function to strip a string to any float contained within it
 def striptonums(string):
     return float(''.join(c for c in string if (c.isdigit() or c == ',')))
 
-#Desired asser weightings (gold, bonds, emerging market stocks, eu value stocks)
+
+# Desired asser weightings (gold, bonds, emerging market stocks, eu value stocks)
 weights = np.array([0.2, 0.2, 0.2, 0.4])
 
-#dataframe for adj close price of assets
 
+# Function to obtain the close price of various assets stored in the asset DataFrame
+def getvalue(assetdf):
 
+    newdata = pd.DataFrame()  # Empty DataFrame for new data
 
-def getvalue(dict):
+    for group in assetdf.columns:
 
-    # df = pd.DataFrame()
-    newdata = pd.DataFrame()
+        for stock in assetdf[group].dropna():  # Iterating over the asset DataFrame ignoring NaN values
 
-    for name, assets in dict.items():
-        total = 0
-
-        for stock in assets:
-
-            ticker = stock.split('#')[0]
-            num = float(striptonums(stock))
-            curr = stock.split('/')[1]
+            ticker = str(stock).split('#')[0]  # Parsing the information contained in each string
+            num = float(striptonums(stock))    #
+            curr = stock.split('/')[1]         #
 
             try:
-                stockdata = pd.DataFrame(web.DataReader(ticker, data_source='yahoo', start=yesterday, end=today)['Adj Close'])
-                stockdata.columns = [ticker + "_price"]
-                stockdata[ticker + "_shares"] = num
+                stockdata = pd.DataFrame(web.DataReader(ticker,
+                                                        data_source='yahoo',
+                                                        start=yesterday,
+                                                        end=today)['Adj Close'])  # Reading the data from Yahoo Finance
 
-                if curr == 'p':
-                    stockdata[ticker + "_price"] /= 100
+                stockdata.columns = [ticker + "_price"]  # Giving a name to the initial column
+                stockdata[ticker + "_shares"] = num  # Adding number of shares to DataFrame
 
-                elif curr == 'e':
-                    stockdata[ticker + "_price"] /= exch('GBP', 'EUR')
+                if curr == 'GBX':
+                    stockdata[ticker + "_price"] /= 100  # Converting to pounds if the currency is pennies
 
-                stockdata[ticker + "_value"] = num*stockdata[ticker + "_price"]
-                newdata = pd.concat([newdata, stockdata], axis=1)
+                elif curr != 'GBP':
+                    stockdata[ticker + "_price"] /= exch('GBP', curr)  # Converting any other currency
+
+                stockdata[ticker + "_value"] = num*stockdata[ticker + "_price"]  # Calculating the value of stocks held
+                newdata = pd.concat([newdata, stockdata], axis=1)  # adding each stockdata to newdata
                 print(stockdata)
                 print("\n")
 
             except KeyError:
-                None
-                # total += stockdata["value"]
-        # df[name][today] = total
-    newdata.index = pd.to_datetime(newdata.index)
+                print(f"Stock {ticker} not found")  # Exception if the data reader cannot find the ticker in question
+                pass
 
-    newdata.to_csv(r'C:\Users\Tom\Desktop\Python projects\Portfolio management tool\newvalues.csv')
-
+    newdata.index = pd.to_datetime(newdata.index)  # Converting the index of the DataFrame to a datetime series
     try:
         olddata = pd.read_csv(r'C:\Users\Tom\Desktop\Python projects\Portfolio management tool\valuedf.csv',
-                          index_col=0,
-                          parse_dates=True,
-                          dayfirst=True)
-    except:
+                              index_col=0,
+                              parse_dates=True,
+                              dayfirst=True,
+                              dtype=float)  # Reading in the old data
+    except Exception:
+        # Exception to create a blank DataFrame if the old data csv file is empty
         olddata = pd.DataFrame()
 
+    # Adding the new data onto the old data
+    olddata = pd.concat([olddata, newdata], axis=0,)
 
-    olddata = pd.concat([olddata,newdata], axis = 0, ignore_index = False)
+    # Filtering out any duplicate rows
+    olddata= olddata[~olddata.index.duplicated(keep="last")]
 
-
+    # Writing the data back into the csv file
     olddata.to_csv(r'C:\Users\Tom\Desktop\Python projects\Portfolio management tool\valuedf.csv')
     return olddata
 
 
 getvalue(myassets)
 
-
-
-
-
-# plt.show()
 
